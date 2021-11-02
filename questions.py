a205cd59538620ba782f708911a705143996d62f
matthewarmetta <mattarmetta@mac.com>
2021-11-01 19:38:54 -0600

    formats and adds new question

--- questions.py
+++ questions.py
@@ -1,9 +1,15 @@
 # Python interview questions
 
 
-######################################################################################
-# Reverse a String (don't use reverse())
-######################################################################################
+# adds 2 numbers
+def add_numbers(n1, n2):
+    return n1 + n2
+
+
+print(add_numbers(18, 20))
+
+
+# reverses a String (don't use reverse())
 def reverse_string(s):
     r_string = ''
     for c in s:
@@ -13,12 +19,9 @@ def reverse_string(s):
 
 
 print(reverse_string('hello world'))
-######################################################################################
 
 
-######################################################################################
-# Given 2 lists of unique numbers where 1 list contains all numbers from the other list minus 1, find the missing number
-######################################################################################
+# given 2 lists of unique numbers where 1 list contains all numbers from the other list minus 1, find the missing number
 def find_missing(full, partial):
     mis_items = set(full) - set(partial)
     return list(mis_items)[0]
@@ -28,10 +31,3 @@ def find_missing(full, partial):
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 list2 = [1, 2, 3, 5, 6, 7, 8, 9]
 print(find_missing(list1, list2))
-######################################################################################
-
-
-######################################################################################
-
-######################################################################################
-
