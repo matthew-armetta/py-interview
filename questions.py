fdf720452fb5b53cbb78c625760a8537769feaa9
matthewarmetta <mattarmetta@mac.com>
2021-11-22 14:21:41 -0700

    fixes file after bad merge

--- questions.py
+++ questions.py
@@ -1,34 +1,42 @@
 # Python interview questions
 
-
-# adds 2 numbers
+# add 2 numbers
 def add_numbers(n1, n2):
-    print('the result is: ')
-    return n1 + n2
+    print(n1 + n2)
 
 
-print(add_numbers(18, 20))
+add_numbers(18, 20)
 
 
-# reverses a String (don't use reverse())
+# reverse string (don't use reverse())
 def reverse_string(s):
     r_string = ''
     for c in s:
         r_string = c + r_string
 
-    return r_string
+    print(r_string)
 
 
-print(reverse_string('hello world'))
+reverse_string('hello world')
 
 
 # given 2 lists of unique numbers where 1 list contains all numbers from the other list minus 1, find the missing number
 def find_missing(full, partial):
     mis_items = set(full) - set(partial)
-    return list(mis_items)[0]
-    print(mis_items)
+    print(list(mis_items)[0])
 
 
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 list2 = [1, 2, 3, 5, 6, 7, 8, 9]
 print(find_missing(list1, list2))
+
+
+# find the average of a list of numbers
+def get_average(numbers):
+    total = 0
+    for n in numbers:
+        total += n
+    print(total / len(numbers))
+
+
+get_average([1, 4, 8, 2, 34, 34, 34, 7, 8, 9])
