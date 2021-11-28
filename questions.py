06b73086d035c7bfa6efd35478e89f35dbc8efbd
jordan <jsmit294>
2021-11-27 19:48:11 -0700

    modifies functions to return values instead of printing them

--- questions.py
+++ questions.py
@@ -5,6 +5,7 @@ import math
 
 
 def add_numbers(n1, n2):
+    return n1 + n2
     print(n1 + n2)
 
 
@@ -16,23 +17,23 @@ def reverse_string(s):
     r_string = ''
     for c in s:
         r_string = c + r_string
+    return r_string
 
-    print(r_string)
 
-
-reverse_string('hello world')
+print(reverse_string('hello world'))
 
 
 # given 2 lists of unique numbers where 1 list contains all numbers from the other list minus 1, find the missing number
 def find_missing(full, partial):
     mis_items = set(full) - set(partial)
-    print(list(mis_items)[0])
+    return list(mis_items)
 
 
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 list2 = [1, 2, 3, 5, 6, 7, 8, 9]
+list3 = [2, 3, 4, 5, 6, 7, 8]
 print(find_missing(list1, list2))
-
+print(find_missing(list1, list3))
 
 # find the average of a list of numbers
 def get_average(numbers):
