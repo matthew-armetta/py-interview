a9ee8779be51d5b0d4a138fb10abc5e4f2c99587
jordan <jsmit294>
2021-11-27 19:47:32 -0700

    adds unit tests

---
+++ unit_tests.py
@@ -0,0 +1,21 @@
+import unittest
+import questions
+
+class QuestionsTestCases(unittest.TestCase):
+
+    # tests the sum of two numbers
+    def test_add_numbers(self):
+        result = questions.add_numbers(44, 99)
+        self.assertEqual(result, 143)
+
+    # tests the reversal of a given string
+    def test_reverse_string(self):
+        result = questions.reverse_string("I'm a hot dog")
+        self.assertEqual(result, "god toh a m'I")
+
+    # tests the comparison of two lists
+    def test_find_missing(self):
+        sample_list = [1, 2, 3, 4, 5]
+        sample_partial_list = [2, 3, 5]
+        result = questions.find_missing(sample_list, sample_partial_list)
+        self.assertEqual(result, [1, 4], 'The function found the missing values.')
