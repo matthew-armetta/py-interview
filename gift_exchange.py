1c6789ab8cb45029fb4d312d8f4a93842a07e2e4
matthew.armetta <mattarmetta@mac.com>
2021-12-02 18:12:50 -0700

    adds feedback to code, look for # feedback

--- gift_exchange.py
+++ gift_exchange.py
@@ -1,12 +1,14 @@
 # This is a sample Python script.
 import random
 
+# feedback: PEP8 convention is to use capital camel case for classnames, so can rename to GiftExchance.py
 
 def main():
     if len(participant_list) == 0:
         print("Let's put together a gift exchange! Add a name to get started.")
         get_names()
-    elif len(participant_list) > 0:
+    elif len(
+            participant_list) > 0:  # feedback: we are allowing only 1 entry but the code breaks if only 1 name is added...
         more_names = input(f"There are {len(participant_list)} participants. Add more? 'Y' or 'N': ")
         if more_names.upper() == 'Y':
             get_names()
@@ -22,17 +24,20 @@ def main():
 participant_list = []
 
 ###  start defining the functions ###
-''' get the list of people participating in the exchange.'''
+''' get the list of people participating in the exchange.'''  # feedback: same as below, put as first line of function
 
 
 def get_names():
     getting_names = True
-    while getting_names == True:
+    while getting_names == True:  # feedback: this variable may not be necessary, just use while True and when "F" in inputted just break
         if len(participant_list) == 0:
-            name = input("Type the name of the first participant: ")
+            name = input(
+                "Type the name of the first participant: ")  # feedback: maybe use a regex to validate input?  i can put blank spaces in now, maybe check all chars are alphas and trim any white space
+            # feedback: maybe allow the user to enter a comma separated list of names as well? can use "split" on input received
             participant_list.append(name)
         else:
-            name = input("Type another name, or -- if you're done -- type 'F': ")
+            name = input(
+                "Type another name, or -- if you're done -- type 'F': ")  # feedback: same regex comment here, how to apply to both?
             if name.upper() == "F":
                 getting_names = False
             else:
@@ -41,10 +46,14 @@ def get_names():
     main()
 
 
-''' Assign the members of the particpant list. '''
+''' Assign the members of the particpant list. '''  # feedback: this docstring should be double not single quotes and be the
 
 
-def matching(givers, receivers):
+# first line of the function, also no space between quotes and words, do u even PEP8 bro, also u do even spell participant
+# correctly bro
+
+
+def matching(givers, receivers):  # feedback: more descriptive function name, matching what?
     results = []
     for name in givers:
         giver = name
@@ -58,7 +67,7 @@ def matching(givers, receivers):
             results.append(giver + ' --> ' + receiver)
             receivers.pop(0)
     print(results)
-    return results
+    return results  # feedback: unecessary return, the value is not used by the caller, the end goal was just to print the info
 
 
 # Press the green button in the gutter to run the script.
