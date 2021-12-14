6da83e30c57015969467125669ca354727ab24b8
matthew.armetta <mattarmetta@mac.com>
2021-12-13 20:58:52 -0700

    adds simple text area to view

--- src/giftexchange/views.py
+++ src/giftexchange/views.py
@@ -5,4 +5,12 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "home page"
+    return "<title>Gift Exchange</title>" \
+           "<body>" \
+           "<div>" \
+           "<p>Welcome to Gift Exchange!</p>" \
+           "<label>Please enter names for the gift exchange:</label>" \
+           "<textarea rows='1' cols='100'>" \
+           "</textarea>" \
+           "</div>" \
+           "</body>"
