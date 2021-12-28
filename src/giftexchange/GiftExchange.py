ab261b04a92ea0b6dbd54665afdab85e47d619d5
jordan <jsmit294>
2021-12-27 20:14:46 -0700

    adds logging to GiftExchange.py

--- src/giftexchange/GiftExchange.py
+++ src/giftexchange/GiftExchange.py
@@ -1,15 +1,19 @@
 import sys
 import random
+import logging
+import timeit
 
 # feedback: PEP8 convention is to use capital camel case for classnames, so can rename to GiftExchance.py
 # locks keyboard and crosses arms.
 
 # empty participant list
 participant_list = []
+start = timeit.timeit()
 
 
 def main():
     begin_game()
+    logging.info(f"Started game at {start}.")
 
 
 def begin_game():
@@ -25,6 +29,7 @@ def begin_game():
 
 def start_gift_exchange(names_for_game):
     more_names = input(f"There are {len(names_for_game)} participants. Add more? 'Y' or 'N': ")
+    logging.debug(f"names_for_games: {names_for_game}, user selected {more_names}")
     return assign_givers(names_for_game) if more_names.upper() == 'N' else get_names_for_games()
 
 
@@ -33,35 +38,39 @@ def get_names_for_games():
     getting_names = True
     while getting_names:
         if len(participant_list) == 0:
-            name = input(
-                "Type the name of the first participant: ").strip()  # strip takes care of the whitespaces...
-            # feedback: maybe allow the user to enter a comma separated list of names as well? can use "split" on input received
+            name = input("Type the name of the first participant: ").strip()  # strip takes care of the whitespaces...
+            # feedback: maybe allow the user to enter a comma separated list of names as well? can use "split" on
+            # input received
             validate_input(name)
         else:
-            name = input(
-                "Type another name, or -- if you're done -- type 'F': ").strip()
+            name = input("Type another name, or -- if you're done -- type 'F': ").strip()
             if name.upper() == "F":
                 getting_names = False
             else:
                 validate_input(name)
     print(f"Here are the participants: {participant_list}.\n")
+    logging.debug(f"Starting game with participants: {participant_list}")
     main()
 
 
 def validate_input(entered_name):
     """ Checks the entered name to make sure it contains only alpha characters and is not a duplicate name. """
-    if entered_name.isalpha() == True and entered_name.capitalize() not in participant_list:
+    if entered_name.isalpha() and entered_name.capitalize() not in participant_list:
         participant_list.append(entered_name.capitalize())
-    elif entered_name.isalpha() == False:
+        logging.debug(f"Added name: {entered_name.capitalize()}.")
+    elif not entered_name.isalpha():
         print(f"'{entered_name}' doesn't look right, try again.")
+        logging.debug(f"Non-alpha name {entered_name} was given.")
         get_names_for_games()
     elif entered_name.capitalize() in participant_list:
         print(f"{entered_name} is already on the list!")
+        logging.debug(f"Name {entered_name.capitalize()} was already on the list.")
         get_names_for_games()
 
 
 def assign_givers(names_for_game):
-    """  Given a list of givers, assigns each item to an item on the list of receivers, and removes that receiver from the receiver list """
+    """  Given a list of givers, assigns each item to an item on the list of receivers, and removes that receiver
+    from the receiver list """
     receivers = random.sample(names_for_game, len(names_for_game))
     results = []
     for name in names_for_game:
@@ -76,6 +85,7 @@ def assign_givers(names_for_game):
             results.append(giver + ' --> ' + receiver)
             receivers.pop(0)
     print(results)
+    logging.info(f"Results are: {results}")
 
     with open("gift_exchange.txt", 'w') as out_file:
         out_file.write("The game will be played as follows: " + str(results))
@@ -91,6 +101,8 @@ def end_game():
         print('....\n' * 10)
         main()
     else:
+        end = timeit.timeit()
+        logging.info(f"Total game time: {end - start}")
         sys.exit("Bye!")
 
 
--- test/giftexchange/test_GiftExchange.py
+++ test/giftexchange/test_GiftExchange.py
@@ -4,10 +4,12 @@ import GiftExchange
 
 class GiftExchangeTests(TestCase):
 
-    def test_validation(self):
+    def test_validate_input(self):
         """ Checks the input of a name to verify alpha characters are entered and duplicates aren't present. """
-        result = GiftExchange.validate_input("marc andre")
-        self.assertNotIn(result, GiftExchange.participant_list)
+        name1 = "marc andre"
+        name2 = "9"
+        GiftExchange.validate_input(name1)
+        self.assertNotIn(name1, GiftExchange.participant_list)
         result = GiftExchange.validate_input("9")
         self.assertNotIn(result, GiftExchange.participant_list)
         GiftExchange.participant_list = ['Jordan']
