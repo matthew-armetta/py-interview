12b0583820258517527c4758f04db39a611a09a7
jordan <jsmit294>
2021-12-27 19:15:42 -0700

    moves tests, fixes messed up function upon which the test depends

--- test/giftexchange/test_GiftExchange.py
+++ test/giftexchange/test_GiftExchange.py
@@ -7,12 +7,11 @@ class GiftExchangeTests(TestCase):
     def test_validation(self):
         """ Checks the input of a name to verify alpha characters are entered and duplicates aren't present. """
         result = GiftExchange.validate_input("marc andre")
-        self.assertEqual(result, False)
+        self.assertNotIn(result, GiftExchange.participant_list)
         result = GiftExchange.validate_input("9")
-        self.assertEqual(result, False)
+        self.assertNotIn(result, GiftExchange.participant_list)
         GiftExchange.participant_list = ['Jordan']
         result = GiftExchange.validate_input('jordan')
-        self.assertEqual(result, False)
+        self.assertEqual(result, len(GiftExchange.participant_list) == 1)
         result = GiftExchange.validate_input('mateusz')
-        self.assertEqual(result, True)
-
+        self.assertIn(result, GiftExchange.participant_list)
